1ac704f9827b8c7d3bf5ebcc84f23612486e3740
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-13 19:06:54 +0530

    [VIGNESH M] Added Player Position

---
+++ PlrPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class PlrPosition
+    {
+        
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Game is played by single player");
+            Console.WriteLine("Player Start Position is:" + position);
+
+        }
+    }
+}
