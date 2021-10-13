095e65d9e1a801fb75b7f3f94886153dcf5c7c24
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-13 18:57:06 +0530

    [VIGNESH M] Added Welcome to Snake and Ladder

---
+++ WelSnakeLadder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class WelSnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder");
+        }
+    }
+}
