6ac1c72df9e060da4426fd421c3f52c720a5d23c
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-13 19:20:12 +0530

    [VIGNESH M] Added Player Roll Die TO Get Random Numbers Between 1 to 6

---
+++ PlrRollDie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class PlrRollDie
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("Number Obtained on Die is:" + die);
+
+        }
+    }
+}
